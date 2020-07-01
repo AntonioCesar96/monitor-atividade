51d8a542abfff292cf27bf3f986449331314246f
Antonio Cesar de Souza Santana <antonio.santana@ms.senac.br>
2020-07-01 08:09:55 -0400

    criação dos projetos front e back

---
+++ back/cpu-activity-monitor/Controllers/MonitorController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace cpu_activity_monitor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MonitorController : ControllerBase
+    {
+        public MonitorController()
+        {
+        }
+    }
+}
+
---
+++ back/cpu-activity-monitor/Hubs/MonitorHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace cpu_activity_monitor.Hubs
+{
+    public class MonitorHub : Hub
+    {
+
+        public async Task ObterMonitor()
+        {
+            Clients.Client(Context.ConnectionId)
+                .SendAsync("ReceberMonitor", retorno);
+        }
+    }
+}
