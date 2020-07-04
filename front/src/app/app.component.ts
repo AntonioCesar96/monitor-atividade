dfc21ef0164756b1d6aa33d3604f31ecc7a337fc
Antonio Cesar de Souza Santana <antonio.santana@ms.senac.br>
2020-07-04 09:49:32 -0400

    adicionado ordenação na tabela

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OrdenarPor } from './ordenar-por.enum';
 import { AppSignalRService } from './services/app-signalr.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
@@ -9,7 +10,14 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit, OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
+  tempoParaBuscarDadosNovamente = 200;
+  enumOrdenarPor = OrdenarPor;
   monitor: any;
+  ordenarPor = OrdenarPor.Cpu;
+  alternarNomeProcesso = false;
+  alternarCpu = false;
+  alternarMemoria = false;
+  alternarThreads = false;
 
   constructor(private signalRService: AppSignalRService) { }
 
@@ -20,14 +28,63 @@ export class AppComponent implements OnInit, OnDestroy {
     this.signalRService
       .receberMonitor()
       .pipe(takeUntil(this.destroy$))
-      .subscribe((monitor) => {
-        this.monitor = monitor;
-        setTimeout(() => {
-          this.signalRService.buscarMonitor();
-        }, 200);
-      });
+      .subscribe((monitor) => this.receberMonitor(monitor));
 
     this.signalRService.buscarMonitor();
+  }
+
+  receberMonitor(monitor) {
+    this.monitor = monitor;
+    this.ordenar(this.ordenarPor);
+
+    setTimeout(() => this.signalRService.buscarMonitor(), this.tempoParaBuscarDadosNovamente);
+  }
+
+  ordenar(ordenarPor: OrdenarPor) {
+    this.ordenarPor = ordenarPor;
+    let campoParaOrdenar: string;
+    let alternar: boolean;
+
+    switch (ordenarPor) {
+      case OrdenarPor.NomeProcesso: {
+        alternar = this.alternarNomeProcesso;
+        campoParaOrdenar = 'nomeDoProcesso';
+        break;
+      }
+      case OrdenarPor.Cpu: {
+        alternar = this.alternarCpu;
+        campoParaOrdenar = 'cpu';
+        break;
+      }
+      case OrdenarPor.Memoria: {
+        alternar = this.alternarMemoria;
+        campoParaOrdenar = 'ram';
+        break;
+      }
+      case OrdenarPor.Threads: {
+        alternar = this.alternarThreads;
+        campoParaOrdenar = 'threads';
+        break;
+      }
+    }
+
+    this.ordenarProcessos(alternar, campoParaOrdenar);
+  }
+
+  ordenarProcessos(alternar, campoParaOrdenar) {
+    if (!this.monitor || !this.monitor.processos) { return; }
+
+    this.monitor.processos.sort((p1, p2) => {
+      if (typeof p1[campoParaOrdenar] === 'string') {
+        return alternar
+        ? p1[campoParaOrdenar].localeCompare(p2[campoParaOrdenar])
+        : p2[campoParaOrdenar].localeCompare(p1[campoParaOrdenar]);
+      }
+
+      return alternar
+      ? p1[campoParaOrdenar] - p2[campoParaOrdenar]
+      : p2[campoParaOrdenar] - p1[campoParaOrdenar];
+    });
   }
 
   ngOnDestroy() {
---
+++ front/src/app/ordenar-por.enum.ts
@@ -0,0 +1,6 @@
+export enum OrdenarPor {
+  NomeProcesso,
+  Cpu,
+  Memoria,
+  Threads
+}
